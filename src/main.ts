a638dbfec8f20227f8cc4ed2efd5e719c39ab546
shogunIncognito <saidsamuel2006@hotmail.com>
2024-03-24 20:40:01 -0500

    test cors

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,29 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const allowedDomains = ['https://maxpanel.vercel.app'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<string>('PORT') || 3000;
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['https://maxpanel.vercel.app'],
+  const options = {
+    origin: function (origin, callback) {
+      console.log(origin);
+
+      if (allowedDomains.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+        return;
+      }
+
+      callback(new Error('Not allowed by CORS'));
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  };
+
+  app.enableCors(options);
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
 
   const config = new DocumentBuilder()
