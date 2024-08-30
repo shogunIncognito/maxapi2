77782a638eb1a41752942991aefbd7d6e1aa025b
shogunIncognito <saidsamuel2006@hotmail.com>
2024-08-30 11:15:16 -0500

    feat: Actualizar el valor predeterminado del rol a "admin" en el modelo de usuario

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,7 +12,7 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: true, default: Role.user })
+  @Prop({ required: true, default: Role.admin })
   role: Role;
 
   @Prop()
